3138aeb14c4b13f915e67acb54c14a914e765df5
Christian Al Barbin <cabaribanchristianal5@gmail.com>
2021-05-31 22:48:17 +0800

    Third Commit

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -13,7 +13,7 @@ trait ApiResponser{
      */
     public function successResponse($data, $code = Response::HTTP_OK){
 
-        return response()->json(['data' => $data, 'site' => 2], $code); //new
+        return response()->json(['data' => $data, 'site' => 1], $code); //new
     }
 
     /**
@@ -24,7 +24,7 @@ trait ApiResponser{
      */
     public function errorResponse($message, $code){
         
-        return response()->json(['error' => $message, 'site' => 2, 'code' => $code], $code); //new
+        return response()->json(['error' => $message, 'site' => 1, 'code' => $code], $code); //new
     }
 
 
